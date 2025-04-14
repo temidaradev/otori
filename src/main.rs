fadc1b0e1e86fd56c8b40cadb80c8db7f5f63f9f
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-04-14 15:37:41 +0300

    input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use std::io::{Write, stdin, stdout};
+
 fn main() {
-    let mut x = 5;
-    println!("Value of x: {}", x);
-    x = 1;
-    println!("Update value of x: {}", x);
+    let mut s = String::new();
+    print!("Please enter some text: ");
+    let _ = stdout().flush();
+    stdin()
+        .read_line(&mut s)
+        .expect("Did not enter a correct string");
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
+    }
+    println!("You typed: {}", s);
 }
