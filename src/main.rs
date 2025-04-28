ff670347f4743b8faaae0ae21b432cd7e5abc321
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-04-28 11:00:15 +0300

    basic calc

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,63 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
+use std::io;
 
 fn main() {
-    let rect = Rectangle { width: 30, height: 50 };
-    let rect2 = Rectangle { width: 10, height: 40 };
-    let rect3 = Rectangle { width: 60, height: 45 };
-    println!("Can rect1 hold rect2? {}", rect.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect.can_hold(&rect3));
-    println!("The area of the rectangle is {} square pixels.", rect.area());
+    let result: f64;
+
+    println!("Enter the first number: ");
+    let x: f64 = input_parser();
+
+    if f64::is_nan(x) {
+        println!("Invalid input!");
+        return;
+    }
+
+    println!("Enter the second number: ");
+    let y: f64 = input_parser();
+
+    if f64::is_nan(y) {
+        println!("Invalid input!");
+        return;
+    }
+
+    println!("List of operators:");
+    println!("(1) Add");
+    println!("(2) Subtract");
+    println!("(3) Multiply");
+    println!("(4) Divide");
+    println!("Select the number associated with the desired operation: ");
+
+    let op: f64 = input_parser();
+
+    if f64::is_nan(op) {
+        println!("Invalid input!");
+        return;
+    }
+
+    let op: i32 = op as i32;
+
+    match op {
+        1 => result = x + y,
+        2 => result = x - y,
+        3 => result = x * y,
+        4 => result = x / y,
+        _ => {
+            println!("Invalid selection");
+            return;
+        }
+    }
+
+    println!("The result is: {}", result);
+}
+
+fn input_parser() -> f64 {
+    let mut x: String = String::new();
+    io::stdin().read_line(&mut x).expect("Invalid Input");
+    let x: f64 = match x.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            return f64::NAN;
+        }
+    };
+
+    return x;
 }
