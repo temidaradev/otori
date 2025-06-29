aa7b9af5297fdf6f690f50db17c928fd66140458
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-06-29 17:10:27 +0300

    able to draw screen and da game

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,8 +1,16 @@
 use chip8_core::*;
-use sdl2::event::Event;
-use std::env;
 
-const SCALE: u32 = 15;
+use sdl2::event::Event;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+const SCALE: u32 = 16;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
 
@@ -23,5 +31,45 @@ fn main() {
         .unwrap();
     let mut canvas = window.into_canvas().present_vsync().build().unwrap();
     canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let mut chip8 = Emu::new();
+    let mut rom = File::open(&args[1]).expect("Unable to open file");
+    let mut buffer = Vec::new();
+    rom.read_to_end(&mut buffer).unwrap();
+    chip8.load(&buffer);
+
+    'gameloop: loop {
+        for evt in event_pump.poll_iter() {
+            match evt {
+                Event::Quit { .. } => {
+                    break 'gameloop;
+                }
+                _ => (),
+            }
+        }
+        chip8.tick();
+        draw_screen(&chip8, &mut canvas);
+    }
+}
+
+fn draw_screen(emu: &Emu, canvas: &mut Canvas<Window>) {
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    let screen_buf = emu.get_display();
+
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    for (i, pixel) in screen_buf.iter().enumerate() {
+        if *pixel {
+            let x = (i % SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
+
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap();
+        }
+    }
     canvas.present();
 }
